ffad4ee93f9da423d221e1e86874882209e270ab
Josh Sizer <joshsizer@gmail.com>
2017-05-15 17:26:36 -0400

    Trying to link opencv_java3.so to my native lib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
+#include "opencv2/core.hpp"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_joshs_daisycv_MainActivity_stringJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    static cv::Mat m;
+    //m.create(4, 5, CV_8UC4);
+
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/joshs/daisycv/MainActivity.java
+++ app/src/main/java/com/example/joshs/daisycv/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.joshs.daisycv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends Activity {
     // Used to load the 'native-lib' library on application startup.
     static {
+        System.loadLibrary("opencv_java3");
+        Log.i("MainActivity", "Loaded opencv_java3");
         System.loadLibrary("native-lib");
     }
 
@@ -14,6 +19,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyActivity", "Got here!");
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
